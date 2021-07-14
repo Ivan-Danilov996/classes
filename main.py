00a1e703e4b87da5876e42170df6c0c922e7772c
Ivan-Danilov996 <61184637+Ivan-Danilov996@users.noreply.github.com>
2021-07-14 17:21:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,119 @@
-class Animal:
-    weights = {}
-    all_weight = 0
-    most_weight = 0
-    most_weight_name = ''
-
-    def __init__(self, name, weight):
-        self.weight = weight
+class Student:
+    def __init__(self, name, surname, gender):
         self.name = name
-        self.add_weights()
-
-    def feed(self):
-        print(self.name, 'fed')
-
-    def add_weights(self):
-        self.weights[self.name] = self.weight
-
-    def get_all_weigth(self):
-        for name, weight in self.weights.items():
-            self.all_weight += weight
-        print(self.all_weight)
-
-    def get_most_weigth(self):
-        for name, weight in self.weights.items():
-            if self.most_weight < weight:
-                self.most_weight = weight
-                self.most_weight_name = name
-        print(self.most_weight_name, self.most_weight)
-
-
-class Cloven_hoofed(Animal):
-    def milk(self):
-        print(self.name, 'milked')
-
-
-class Bird(Animal):
-    def collect_eggs(self):
-        print(self.name, 'eggs harvested successfully')
-
-
-class Goose(Bird):
-    vote = 'krya'
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        self.average_rating = 0
+    def rate(self, mentor, course, grade):
+        if isinstance(
+                mentor, Lecturer
+        ) and course in self.courses_in_progress and course in mentor.courses_attached:
+            if course in mentor.grades:
+                mentor.grades[course] += [grade]
+            else:
+                mentor.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+    def __str__(self):
+        all_grades = []
+        sum_grades = 0
+        all_courses = []
+        for course, grades in self.grades.items():
+            all_grades.extend(grades)
+            all_courses.append(course)
+        for grade in all_grades:
+          sum_grades += grade
+        self.average_rating = sum_grades / len(all_grades)
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания  {self.average_rating}\nКурсы в процессе изучения: {''.join(all_courses)}\nЗавершенные курсы: {''.join(self.finished_courses)}"
+    def __lt__(self, other):
+        return self.average_rating < other.average_rating
 
 
-class Cow(Cloven_hoofed):
-    vote = 'moo'
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+        self.average_rating = 0
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"      
 
 
-class Goat(Cloven_hoofed):
-    vote = 'be'
+class Lecturer(Mentor):
+    def __str__(self):
+        all_grades = []
+        sum_grades = 0
+        for course, grades in self.grades.items():
+            all_grades.extend(grades)
+        for grade in all_grades:
+          sum_grades += grade
+        self.average_rating = sum_grades / len(all_grades)  
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rating}" 
+    def __lt__(self, other):
+        return self.average_rating < other.average_rating
 
 
-class Sheep(Cloven_hoofed):
-    vote = 'be'
-
-    def to_cut(self):
-        print(self.name, 'tonsured')
-
-
-class Chicken(Bird):
-    vote = 'krya'
-
-
-class Duck(Bird):
-    vote = 'krya'
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(
+                student, Student
+        ) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
-white = Goose('White', 5)
-gray = Goose('Gray', 4)
-manka = Cow('Manka', 500)
-barashek = Sheep('Barashek', 30)
-kudryavy = Sheep('Kudryavy', 30)
-ko_ko = Chicken('Ko_ko', 2)
-kukareku = Chicken('Kukareku', 1)
-horns = Goat('Horns', 20)
-hooves = Goat('Рooves', 25)
-kryackva = Duck('Kryackva', 5)
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
 
-white.feed()
-manka.milk()
-print(Animal.weights)
-white.get_all_weigth()
-white.get_most_weigth()
+bad_student = Student('Jack', 'Black', 'your_gender')
+bad_student.courses_in_progress += ['Python']
+
+
+
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+
+cool_mentor.rate_hw(bad_student, 'Python', 6)
+cool_mentor.rate_hw(bad_student, 'Python', 3)
+cool_mentor.rate_hw(bad_student, 'Python', 2)
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+cool_lecturer = Lecturer('Ivan', 'Ivanov')
+cool_lecturer.courses_attached += ['Python']
+
+best_student.rate(cool_lecturer, 'Python', 10)
+best_student.rate(cool_lecturer, 'Python', 2)
+best_student.rate(cool_lecturer, 'Python', 1)
+print(cool_lecturer.grades)
+
+print(best_student.grades)
+
+print(str(best_student))
+print(str(cool_mentor))
+print(str(cool_lecturer))
+
+#Задание №4 функция для лекторов и студентов
+
+def get_average_rating(students_list, course_name):
+  sum_grades = 0
+  all_length = 0
+  for student in students_list:
+    all_length += len(student.grades[course_name])
+    for grade in student.grades[course_name]:
+      sum_grades += grade
+  return sum_grades / all_length
+
+
+print(get_average_rating([cool_lecturer], 'Python')) 
